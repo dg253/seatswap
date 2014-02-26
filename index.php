48dd040671e8eed3ce09c26ff8e7ffd5be30e672
dg253 <dg253@njit.edu>
2014-02-26 11:49:17 -0500

    setup program structure and functions

--- index.php
+++ index.php
@@ -7,7 +7,86 @@ Date: Wed, February 26,2014
 Note: Print 1-100, multiples of 3 print "Seat", multiples of 5 print "Swap", multiples of 3&5 print "SeatSwap"
 */
 
+$program = new program();
 
+class program
+{
+    function __construct()
+    {
+        $page = 'homepage';
+        $arg  = NULL;
+        if (isset($_REQUEST['page'])) {
+            $page = $_REQUEST['page'];
+        }
+        if (isset($_REQUEST['arg'])) {
+            $arg = $_REQUEST['arg'];
+        }
+    
+        $page = new $page($arg);
+    }
 
+    function __destruct()
+    {
+    }
+}
+
+abstract class page {
+    public $content;
+
+    function menu()
+    {
+        //menu for the homepage to display
+        $menu = '<a href="./index.php?page=homepage">Home</a> </br>';
+        $menu .= '<a href="./index.php?page=code"></a>SeatSwap Code</br>';
+        return $menu;
+    }
+    
+    function __construct($arg = NULL)
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $this->get();
+        } else {
+            $this->post();
+        }
+    }
+
+    function get()
+    {
+    }
+
+    function post()
+    {
+    }
+
+    function __destruct()
+    {   
+        echo $this->content;
+    }
+	
+	function numberSorter(){
+	//perform loop
+	
+	//return multiples of 3
+	
+	//returns muliples of 5
+	
+	//return multiples of 3&5
+	}
+}
+
+class homepage extends page {
+    function get()
+    {
+    $this->content .= $this->menu();
+    }
+}
+
+class code extends page {
+    function get()
+    {
+    $this->content .= $this->menu();
+    $this->content .= $this->numberSorter();
+    }
+}
 
 ?>

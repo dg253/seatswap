7f674fa2ecb2c7eb221e06a77a03c9527862180b
dg253 <dg253@njit.edu>
2014-02-26 10:56:02 -0500

    update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-seatSwap PHP Code
+SeatSwap PHP Code
 Author: David Garcia
 Date: Wed, February 26,2014
 Note: Print 1-100, multiples of 3 print "Seat", multiples of 5 print "Swap", multiples of 3&5 print "SeatSwap"

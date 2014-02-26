2bb307551aaf0a73a60bb9964f51501f637af741
dg253 <dg253@njit.edu>
2014-02-26 10:53:45 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+seatSwap PHP Code
+Author: David Garcia
+Date: Wed, February 26,2014
+Note: Print 1-100, multiples of 3 print "Seat", multiples of 5 print "Swap", multiples of 3&5 print "SeatSwap"
+*/
+
+
+
+
+?>
